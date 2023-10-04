41f814d6dddb1ddb8340293a26b4839efad5c026
Vanessaa4793 <146861977+Vanessaa4793@users.noreply.github.com>
2023-10-04 11:33:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Vanessa Archer"; // update the part BETWEEN the "quotes"
+let userName = "Vanessaa4793"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
